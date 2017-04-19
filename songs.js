6edc02c621bbbe9aa6a186d7ef4891b349ba887c
n.herron <nherron@nherrons-MacBook-Pro.local>
2017-04-18 19:17:08 -0500

    added to Dom want to figure out styling

---
+++ songs.js
@@ -0,0 +1,61 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+songs.unshift("In Bloom - by Nirvana on the album Nevermind");
+songs.push("Fluffy - by Wolf Alice on the album My Love is Cool");
+
+console.log(songs);
+
+//looping and replacing
+for (var i = 0; i < songs.length; i++) {
+	songs[i] = songs[i].replace("*", "");
+	songs[i] = songs[i].replace(/ > /g, " - ");
+	songs[i] = songs[i].replace("(", "");
+	songs[i] = songs[i].replace("!", "");
+	songs[i] = songs[i].replace("@", "");
+}
+console.log("newArray",songs);
+
+document.getElementById("main").innerHTML = songs;
+
+
+
+
+
+/// adding to the DOM
+/// quotesArray.forEach(function(quote, index) {//grab first item, here's what index it is
+	// var quoteBlock = `<div id="quote--${index}">
+	// 					<h3>"${quote}" - Star Wars</h3>
+	// 					<p>this is awesome</p>
+	// 				  </div>`; //whatever's inside brackets will be evaluatesd
+
+// document.getElementById("main").innerHTML = songs;
+// var arrayBlock = `<section>
+// 					<p class="songName"> </p>
+// 					<p>Nirvana | Nevermind | Rock</p>
+// 				</section>
+// 				<section>
+// 					<p class="songName">Fluffy</p>
+// 					<p>Wolf Alice | My Love is Cool | Rock</p>
+// 				</section>
+// 				<section>
+// 					<p class="songName">Exodus</p>
+// 					<p>Bob Marley | Exodus | Reggae</p>
+// 				</section>
+// 				<section>
+// 					<p class="songName">'Round Midnight</p>
+// 					<p> Grant Green | Green Street | Jazz</p>
+// 				</section>
+
+
+// // sonnet = sonnet.replace("winter", "yuletide");
+// // console.log("replace winter", sonnet);
+
+// // // Replace all occurances of the string "the" with "a large".
+// // sonnet = sonnet.replace(/the /gi, "a large ");
+// // console.log("replace the", sonnet);
